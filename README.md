aabccbfd43d5121bb3ec890dc54d95d0bb53d70a
Saúl Caballero <saul.caballero.ramirez@gmail.com>
2018-05-22 18:04:11 -0500

    Most of the work is done.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# hit_run
+# Hit & Run Algorithm implemented with GPGPUS
+
+Authors:
+- Mario Vazquez Corte
+- Saúl Caballero Ramírez
+
+TODO
+
+- [x] Chebyshev Center
+- [x] Matrix multiplication & invertion in CUDA
+- [ ] Find borders of the polytope
+- [ ] Final Sampler
---
+++ hit_run.c
@@ -0,0 +1,20 @@
+#include "Headers/init.h"
+#include "Headers/chebyshev_center.h"
+#include "Headers/matrix_operations.h"
+
+int main(int argc, char *argv[]) {
+  parse_arguments(argc, argv);
+
+  define_pointers();
+  define_parameters();
+
+  get_initvalue();
+
+
+  // Free mallocs
+  free(P_EQUALITY->a);
+  free(P_INEQUALITY->a);
+  free(P_EQUALITY->b);
+  free(P_INEQUALITY->b);
+  return 0;
+}
